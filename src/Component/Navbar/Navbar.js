c8ebf217ad578328349c579cd5d68f93b9c8be50
Your Name~ <you@example.com>
2022-07-26 15:13:39 +0100

    navbar elements styled

--- src/Component/Navbar/Navbar.js
+++ src/Component/Navbar/Navbar.js
@@ -1,28 +1,33 @@
 import React from 'react'
+import './Navbar.css'
 import { Link } from "react-router-dom"
 
 function Navbar() {
   return (
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to='/'>Home</Link>
-          </li>
-          <li>
-            <Link to='/packing-list'>Packing List</Link>
-          </li>
-          <li>
-            <Link to='/weather'>Weather Forecast</Link>
-          </li>
-          <li>
-            <Link to='/exchange'>Exchange Rates</Link>
-          </li>
-          <li>
-            <Link to='/contacts'>Emergency Contacts</Link>
-          </li>
-        </ul>
-      </nav>
+    <div className='menu-styles' >
+      <div className='logo'> logo</div>
+       
+        <div>
+          <nav className='main-menu'>
+            <ul >
+              <li>
+                <Link to='/'>Home</Link>
+              </li>
+              <li>
+                <Link to='/packing-list'>Packing List</Link>
+              </li>
+              <li>
+                <Link to='/weather'>Weather Forecast</Link>
+              </li>
+              <li>
+                <Link to='/exchange'>Exchange Rates</Link>
+              </li>
+              <li>
+                <Link to='/contacts'>Emergency Contacts</Link>
+              </li>
+            </ul>
+          </nav>
+       </div>
     </div>
   )
 }
