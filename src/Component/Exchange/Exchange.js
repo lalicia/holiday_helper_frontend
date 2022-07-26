7d76936b4940e36d5f323b0fa716dd39683d3027
mateuszsoszynski <soszynski94@googlemail.com>
2022-07-26 15:56:39 +0100

    timeseries api fetched and tested

--- src/Component/Exchange/Exchange.js
+++ src/Component/Exchange/Exchange.js
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from 'react'
 function Exchange() {
 
   const [url, setUrl] = useState(``)
+  const [historical, setHistorical] = useState("")
   const [to, setTo] = useState("")
   const [from, setFrom] = useState("")
   const [amount, setAmount] = useState("")
@@ -23,19 +24,27 @@ function Exchange() {
   useEffect(() => {
     async function getRates() {
       const response = await fetch(url, requestOptions)
+      const response2 = await fetch(historical, requestOptions)
 
       let data = await response.json()
+      let data2 = await response2.json()
   
       console.log(data)
+      console.log(data2)
       setResult(data.result)
       setRate(data.info.rate)
     }
     getRates()
   }, [url])
+
+  // fetch("https://api.apilayer.com/fixer/timeseries?start_date={start_date}&end_date={end_date}", requestOptions)
+
+
   
   
   function handleClick() {
     setUrl(`https://api.apilayer.com/fixer/convert?to=${to}&from=${from}&amount=${amount}`)
+    setHistorical(`https://api.apilayer.com/fixer/timeseries?start_date=2022-07-18&end_date=2022-07-25&base=${from}&symbols=${to}`)
   }
 
   return (
