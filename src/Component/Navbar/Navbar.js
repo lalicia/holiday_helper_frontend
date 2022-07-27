32c25aab312367a83b4f02a5edf1c9324e6ec6fc
Your Name~ <you@example.com>
2022-07-27 10:19:28 +0100

    navbar is still in progress the icon displays will need to add more css to complete the fuctionality

--- src/Component/Navbar/Navbar.js
+++ src/Component/Navbar/Navbar.js
@@ -1,15 +1,25 @@
-import React from 'react'
-import './Navbar.css'
+import React, {useState} from 'react'
+import './Navbar.css';
 import { Link } from "react-router-dom"
+// import Hamburger from 'hamburger-react'
+import { Close, MenuOutlined } from '@material-ui/icons'
+
 
 function Navbar() {
+  const [isOpen, setOpen] = useState(false)
+  const DisplayMenu = () => {
+    setOpen(!isOpen)
+  }
   return (
     <div className='menu-styles' >
       <div className='logo'> logo</div>
        
-        <div>
-          <nav className='main-menu'>
-            <ul >
+        <div className='menu'>
+          <nav>
+          <ul >
+            <div className='closed'>
+              <Close className='closed' onClick={DisplayMenu} />
+              </div>
               <li>
                 <Link to='/'>Home</Link>
               </li>
@@ -26,7 +36,12 @@ function Navbar() {
                 <Link to='/contacts'>Emergency Contacts</Link>
               </li>
             </ul>
-          </nav>
+        </nav>
+        <div className="menubar">
+          <MenuOutlined className='menu' onClick={DisplayMenu} />
+        </div>
+
+        
        </div>
     </div>
   )
