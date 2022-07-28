b8dcde16a005678d8479d6b50eb76848facb45b2
mateuszsoszynski <soszynski94@googlemail.com>
2022-07-28 10:14:37 +0100

    Navbar

--- src/Component/Navbar/Navbar.js
+++ src/Component/Navbar/Navbar.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react'
-import './navbar.css';
+import './Navbar.css';
 import { Link } from "react-router-dom"
 // import Hamburger from 'hamburger-react'
 // import { Close, MenuOutlined } from '@material-ui/icons'
