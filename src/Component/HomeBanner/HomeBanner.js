3170a113241faead3be3aa566695ec1bf46d9af4
Harry Evans <harryevans177@gmail.com>
2022-08-02 09:38:46 +0100

    changed import name for svg as it was throwing error

--- src/Component/HomeBanner/HomeBanner.js
+++ src/Component/HomeBanner/HomeBanner.js
@@ -1,13 +1,11 @@
 import React from "react";
 import "./HomeBanner.css";
-import Banner from'../../Assets/images/BannerImageCropped.svg'
+import Banner from "../../Assets/images/BannerImage.svg";
 
 function HomeBanner() {
   return (
     <div className="HomeBanner">
-      <img className="Banner"
-            src={Banner} alt="Banner"
-          />
+      <img className="Banner" src={Banner} alt="Banner" />
     </div>
   );
 }
