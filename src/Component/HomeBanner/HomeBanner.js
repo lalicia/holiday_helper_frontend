25b17c1bd6034e89a28b3449a8125b2689730c3b
mandark1 <amandalwhittaker@yahoo.co.uk>
2022-08-02 09:19:18 +0100

    changed banner image to SVG

--- src/Component/HomeBanner/HomeBanner.js
+++ src/Component/HomeBanner/HomeBanner.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./HomeBanner.css";
-import Banner from'../../Assets/images/Banner1.png'
+import Banner from'../../Assets/images/BannerImageCropped.svg'
 
 function HomeBanner() {
   return (
     <div className="HomeBanner">
       <img className="Banner"
-            src={Banner} alt="Banner Image"
+            src={Banner} alt="Banner"
           />
     </div>
   );
