b7c761550c4ca31c3fc2073a6da3c730a610008d
mandark1 <amandalwhittaker@yahoo.co.uk>
2022-08-03 11:20:23 +0100

    fixed footer problem. removed padding. tweaked header scrolling css fix

--- src/App/App.js
+++ src/App/App.js
@@ -8,18 +8,21 @@ import Contacts from "../Component/Contacts/Contacts";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from "../Component/Footer/Footer";
 
+/* added extra class around navbar and body of app to keep footer separate so we can pad at the bottom to stop footer covering content */
 function App() {
   return (
     <div className="App">
       <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/packing-list" element={<TodoList />} />
-          <Route path="/exchange" element={<Exchange />} />
-          <Route path="/weather" element={<Weather />} />
-          <Route path="/contacts" element={<Contacts />} />
-        </Routes>
+        <div className="main-body">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/packing-list" element={<TodoList />} />
+            <Route path="/exchange" element={<Exchange />} />
+            <Route path="/weather" element={<Weather />} />
+            <Route path="/contacts" element={<Contacts />} />
+          </Routes>
+        </div>
         <Footer />
       </Router>
     </div>
