9b2e1d59e4d078b2747e0d3cae2e7d1b2fd6ae88
Your Name~ <you@example.com>
2022-08-08 09:47:19 +0100

    created authentication added error catching when usere tried to signup with the same email or enters wrong password, added condtional render for when signed up it loads and user can't spam the signup function added contional render for when user logsin the signup funtion is disable

---
+++ src/authentication/authentication.js
@@ -0,0 +1,59 @@
+import { useRef, useState } from "react";
+
+import { signup, login, logout, useAuth } from "../../firebase-config";
+
+function Authentication() {
+  const [ loading, setLoading ] = useState(false);
+  const currentUser = useAuth();
+
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  //fuction that handles singup 
+  async function handleSignup() {
+    setLoading(true);
+    try {
+      await signup(emailRef.current.value, passwordRef.current.value);
+    } catch {
+      alert("Error this email already exists!"); //if 
+    }
+    setLoading(false);
+  }
+//function that handles login process 
+  async function handleLogin() {
+    setLoading(true);
+    try {
+      await login(emailRef.current.value, passwordRef.current.value);
+    } catch {
+      alert("Error wrong Email/password!");
+    }
+    setLoading(false);
+  }
+
+  async function handleLogout() {
+    setLoading(true);
+    try {
+      await logout();
+    } catch {
+      alert("Error!");
+    }
+    setLoading(false);
+  }
+
+  return (
+    <div id="main">
+      
+      <div>Currently logged in as: { currentUser?.email } </div>
+
+      <div id="fields">
+        <input ref={emailRef} placeholder="Email" />
+        <input ref={passwordRef} type="password" placeholder="Password" />
+      </div>
+      //conditional render if user is loged in disable login/signup 
+      <button disabled={ loading || currentUser } onClick={handleSignup}>Sign Up</button>
+      <button disabled={ loading || currentUser } onClick={handleLogin}>Log In</button>
+      <button disabled={ loading || !currentUser } onClick={handleLogout}>Log Out</button>
+
+    </div>
+  );
+}
+export default Authentication;
