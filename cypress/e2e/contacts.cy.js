cb9c654d973f1fa882cd66def7440e9a405b82a6
Jed <jedhumphries@hotmail.co.uk>
2022-08-09 16:20:16 +0100

    updated test for emergency contacts

--- cypress/e2e/contacts.cy.js
+++ cypress/e2e/contacts.cy.js
@@ -1,25 +1,26 @@
 describe('emergency contacts page', () => {
   it('test functionalty of emergency contacts page page', () => {
     
-    cy.wait(2000)
     cy.viewport(375, 812)
     cy.visit('http://localhost:3000/contacts')
     cy.get('.page-title').contains('Emergency Contacts')
-    cy.get('.AZDirectory > .table-header').should('contain', 'Name').and('contain', 'Fire').and('contain', 'Ambulance').and('contain', 'Police').and('contain', 'Embassy')
+    cy.get('.az-table-headers').should('contain', 'Country').and('contain', 'Fire').and('contain', 'Ambulance').and('contain', 'Police').and('contain', 'Embassy')
     cy.get('.form-select').select('Brazil')
     cy.get('.country-title').contains('Brazil')
-    cy.get('.emergency-component-div > :nth-child(2)').contains('Fire: 193')
-    cy.get('.emergency-component-div > :nth-child(3)').contains('Police: 190')
-    cy.get('.emergency-component-div > :nth-child(4)').contains('Ambulance: 192')
-    cy.get('.emergency-component-div > :nth-child(5)').contains('Embassy: +55 6133292300')
-    cy.get('.AZDirectory > .columns-div').should('contain', '193').and('contain', '190').and('contain', '192').and('contain', '+55 6133292300')
+    cy.get('.emergency-component-div > :nth-child(3)').contains('Fire: 193')
+    cy.get('.emergency-component-div > :nth-child(4)').contains('Police: 190')
+    cy.get('.emergency-component-div > :nth-child(5)').contains('Ambulance: 192')
+    cy.get('.emergency-component-div > :nth-child(6)').contains('Embassy: +55 6133292300')
+    cy.get('.az-table').should('contain', '193').and('contain', '190').and('contain', '192').and('contain', '+55 6133292300')
+    
     cy.get('.form-select').select('Germany')
     cy.get('.country-title').contains('Germany')
-    cy.get('.emergency-component-div > :nth-child(2)').contains('Fire: 115')
-    cy.get('.emergency-component-div > :nth-child(3)').contains('Police: 113')
-    cy.get('.emergency-component-div > :nth-child(4)').contains('Ambulance: 118')
-    cy.get('.emergency-component-div > :nth-child(5)').contains('Embassy: +33 0144513100')
-    cy.get('.AZDirectory > .columns-div').should('contain', '115').and('contain', '113').and('contain', '118').and('contain', '+33 0144513100')
+    cy.get('.emergency-component-div > :nth-child(3)').contains('Fire: 115')
+    cy.get('.emergency-component-div > :nth-child(4)').contains('Police: 113')
+    cy.get('.emergency-component-div > :nth-child(5)').contains('Ambulance: 118')
+    cy.get('.emergency-component-div > :nth-child(6)').contains('Embassy: +33 0144513100')
+    cy.get('.az-table').should('contain', '115').and('contain', '113').and('contain', '118').and('contain', '+33 0144513100')
+    
    
   })
 })
